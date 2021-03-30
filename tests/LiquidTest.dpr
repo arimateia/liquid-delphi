9374999bf112c47a3f57fc2860b0bc93197fe6ee
arimateia <arimateia@outlook.com>
2021-03-30 14:06:25 -0300

    Directory structure changed

--- tests/LiquidTest.dpr
+++ tests/LiquidTest.dpr
@@ -17,21 +17,21 @@ program LiquidTest;
 uses
   DUnitTestRunner,
   TestLiquid in 'TestLiquid.pas',
-  Liquid.Template in '..\Liquid.Template.pas',
-  Liquid.Default in '..\Liquid.Default.pas',
-  Liquid.Context in '..\Liquid.Context.pas',
-  Liquid.Interfaces in '..\Liquid.Interfaces.pas',
-  Liquid.Tag in '..\Liquid.Tag.pas',
-  Liquid.Block in '..\Liquid.Block.pas',
-  Liquid.Exceptions in '..\Liquid.Exceptions.pas',
-  Liquid.Variable in '..\Liquid.Variable.pas',
-  Liquid.Document in '..\Liquid.Document.pas',
-  Liquid.Hash in '..\Liquid.Hash.pas',
-  Liquid.Tuples in '..\Liquid.Tuples.pas',
-  Liquid.Condition in '..\Liquid.Condition.pas',
-  Liquid.Utils in '..\Liquid.Utils.pas',
-  Liquid.Tags in '..\Liquid.Tags.pas',
-  Liquid.Filters in '..\Liquid.Filters.pas';
+  Liquid.Block in '..\src\Liquid.Block.pas',
+  Liquid.Condition in '..\src\Liquid.Condition.pas',
+  Liquid.Context in '..\src\Liquid.Context.pas',
+  Liquid.Default in '..\src\Liquid.Default.pas',
+  Liquid.Document in '..\src\Liquid.Document.pas',
+  Liquid.Exceptions in '..\src\Liquid.Exceptions.pas',
+  Liquid.Filters in '..\src\Liquid.Filters.pas',
+  Liquid.Hash in '..\src\Liquid.Hash.pas',
+  Liquid.Interfaces in '..\src\Liquid.Interfaces.pas',
+  Liquid.Tag in '..\src\Liquid.Tag.pas',
+  Liquid.Tags in '..\src\Liquid.Tags.pas',
+  Liquid.Template in '..\src\Liquid.Template.pas',
+  Liquid.Tuples in '..\src\Liquid.Tuples.pas',
+  Liquid.Utils in '..\src\Liquid.Utils.pas',
+  Liquid.Variable in '..\src\Liquid.Variable.pas';
 
 {$R *.RES}
 
